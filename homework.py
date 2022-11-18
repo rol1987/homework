559d7e04871cb7350d9e10bdbb6bd69e2cc19252
sergey korolev <rol1987@yandex.ru>
2022-11-18 20:11:47 +0300

    Реализовал метод выставления оценок лекторам у класса Student

--- homework.py
+++ homework.py
@@ -6,6 +6,15 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
         
 class Mentor:
     def __init__(self, name, surname):
@@ -14,7 +23,7 @@ class Mentor:
         self.courses_attached = []
  
 class Lecturer(Mentor):
-    pass
+    grades = {}
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -26,14 +35,16 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+best_student = Student('Иван', 'Иванович', 'М')
+best_student.courses_in_progress += ['Excel']
+best_student.courses_in_progress += ['Java']
+
+best_lec = Lecturer('Роман', 'Романович')
+best_lec.courses_attached += ['Excel']
+best_lec.courses_attached += ['Java']
  
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-print(best_student.grades)
+best_student.rate_lecturer(best_lec, 'Excel', 9)
+best_student.rate_lecturer(best_lec, 'Java', 9)
+best_student.rate_lecturer(best_lec, 'Python', 10)
+
+print(best_lec.grades)
